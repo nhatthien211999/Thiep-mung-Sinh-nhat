ef3915fafecb3230746d3d5a9c41316afa7e8dbb
Le Dao Nhat Thien <63295366+nhatthien211999@users.noreply.github.com>
2023-12-24 14:59:28 +0700

    Add files via upload
    create card happy birthday

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'My';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100010278810584';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người con gái xinh đẹp nhất hôm nay';
+const contentCard = 'Chúc bạn sinh nhật tràn ngập ánh nắng, tiếng cười, tình yêu và hạnh phúc.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
